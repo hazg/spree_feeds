527fa26b09c30eb967d19589b72ed8b548b27046
Aleksandr Aleksandrov <hazg@mail.ru>
2019-07-08 10:21:45 +0300

    U

---
+++ app/overrides/add_feeds_link.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+  name: "spree-feeds-add-feeds-link",
+  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
+  text: '<%= configurations_sidebar_menu_item Spree.t(:taxon_mapping), admin_taxon_mappings_path %>',
+  disabled: false
+)
